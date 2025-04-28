d3b333aa77b5b3d8904b1db77cfd75dc8ea312d1
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-28 10:21:21 +0200

    added await to sleep function call
    and added removeTodo as well updateTodo

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -5,16 +5,24 @@ import { Todo } from '../model/todo.model';
 @Injectable({ providedIn: 'root' })
 export class TodosService {
   async getTodos() {
-    sleep(1000);
+    await sleep(1000);
     return TODOS;
   }
 
   async addTodo(todo: Partial<Todo>): Promise<Todo> {
-    sleep(1000);
+    await sleep(1000);
     return { id: Math.random().toString(36).substring(2, 9), ...todo } as Todo;
+  }
+
+  async removeTodo(id: string) {
+    await sleep(500);
+  }
+
+  async updateTodo(id: string, completed: boolean) {
+    await sleep(500);
   }
 }
 
-async function sleep(ms: number): Promise<void> {
+async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
