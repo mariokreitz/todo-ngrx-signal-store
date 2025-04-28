e42ca5961e233ae7af0f3d6477d914594c5f7691
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-28 08:40:32 +0200

    loading todos from store on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TodoStore } from './store/todos.store';
+import { CommonModule } from '@angular/common';
+import { TodosListComponent } from './todos-list/todos-list.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, TodosListComponent, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -11,6 +14,12 @@ export class AppComponent implements OnInit {
   store = inject(TodoStore);
 
   ngOnInit(): void {
-    this.store.todos();
+    this.loadTodos().then(() => {
+      console.log('Todos loaded');
+    });
+  }
+
+  async loadTodos() {
+    await this.store.loadAll();
   }
 }
