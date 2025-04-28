ecda129c3664357c1e20ea0acacdd27f6b30c1cb
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-28 10:20:59 +0200

    changed id from number to string

--- src/app/model/mock-data.ts
+++ src/app/model/mock-data.ts
@@ -1,12 +1,12 @@
 import { Todo } from './todo.model';
 
 export const TODOS: Todo[] = [
-  { id: 1, title: 'Learn Ngrx', completed: false },
-  { id: 2, title: 'Learn Signal', completed: true },
-  { id: 3, title: 'Learn Mock Data', completed: true },
-  { id: 4, title: 'Create Mock Data', completed: false },
-  { id: 5, title: 'Add Todo Items', completed: false },
-  { id: 6, title: 'remove Todo Items', completed: false },
-  { id: 7, title: 'Edit Todo Items', completed: false },
-  { id: 8, title: 'Complete Todo Items', completed: false },
+  { id: '1', title: 'Learn Ngrx', completed: false },
+  { id: '2', title: 'Learn Signal', completed: true },
+  { id: '3', title: 'Learn Mock Data', completed: true },
+  { id: '4', title: 'Create Mock Data', completed: false },
+  { id: '5', title: 'Add Todo Items', completed: false },
+  { id: '6', title: 'remove Todo Items', completed: false },
+  { id: '7', title: 'Edit Todo Items', completed: false },
+  { id: '8', title: 'Complete Todo Items', completed: false },
 ];
