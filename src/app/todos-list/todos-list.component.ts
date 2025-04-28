defda67ac1ed6f6e8555cdcccaf82ede0c40a987
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-28 10:22:52 +0200

    added constructor effect for filter signal
    and added NgStyle for conditional styling as well function wrapper to update todos in store

--- src/app/todos-list/todos-list.component.ts
+++ src/app/todos-list/todos-list.component.ts
@@ -1,12 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, viewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {
+  MatButtonToggleChange,
+  MatButtonToggleGroup,
+  MatButtonToggleModule,
+} from '@angular/material/button-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
-import { TodoStore } from '../store/todos.store';
+import { TodosFilter, TodoStore } from '../store/todos.store';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'todos-list',
@@ -18,6 +23,7 @@ import { TodoStore } from '../store/todos.store';
     MatSelectModule,
     MatListModule,
     MatInputModule,
+    NgStyle,
   ],
   templateUrl: './todos-list.component.html',
   styleUrl: './todos-list.component.scss',
@@ -25,7 +31,30 @@ import { TodoStore } from '../store/todos.store';
 export class TodosListComponent {
   store = inject(TodoStore);
 
+  filter = viewChild.required(MatButtonToggleGroup);
+
+  constructor() {
+    effect(() => {
+      const filter = this.filter();
+      filter.value = this.store.filter();
+    });
+  }
+
   async onAddTodo(title: string) {
     await this.store.addTodo(title);
+  }
+
+  async onRemoveTodo(id: string, event: MouseEvent) {
+    event.stopPropagation();
+    await this.store.removeTodo(id);
+  }
+
+  async onTodoToggled(id: string, completed: boolean) {
+    this.store.updateTodo(id, completed);
+  }
+
+  onFilterTodo(event: MatButtonToggleChange) {
+    const filter = event.value as TodosFilter;
+    this.store.updateFilter(filter);
   }
 }
